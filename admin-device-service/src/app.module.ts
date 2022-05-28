f865f0b6a36687d1fac87d18701fadb76b6cfefc
Jung Ian <jay2u8809@gmail.com>
2022-05-29 00:22:35 +0900

    Feat. add modules (auth, user, device-management)

--- admin-device-service/src/app.module.ts
+++ admin-device-service/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { AuthtController } from './autht/autht.controller';
+import { UserModule } from './user/user.module';
+import { DeviceManagementModule } from './device-management/device-management.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [AuthModule, UserModule, DeviceManagementModule],
+  controllers: [AppController, AuthtController],
   providers: [AppService],
 })
 export class AppModule {}
